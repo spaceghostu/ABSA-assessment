6f2834737186d33b39b0f91e1fcddc4f011e7e2e
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2020-07-21 13:22:57 +0200

    added header component

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { UiComponent } from './ui.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [UiComponent],
+  declarations: [
+    UiComponent,
+    HeaderComponent,
+  ],
   imports: [
   ],
-  exports: [UiComponent]
+  exports: [
+    UiComponent,
+    HeaderComponent,
+  ]
 })
 export class UiModule { }
