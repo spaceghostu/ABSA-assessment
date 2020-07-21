e30774d205c639901348c097791679c73475fbaa
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2020-07-21 13:57:48 +0200

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from '@ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { UiModule } from '@ui';
     BrowserModule,
     AppRoutingModule,
     UiModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
