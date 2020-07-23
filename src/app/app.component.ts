cc7e6122b16dbd559409e2d5defd30c800573a4a
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2020-07-23 13:37:27 +0200

    preload images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'absa-assessment';
+export class AppComponent implements OnInit {
+  images = new Array();
+
+  ngOnInit() {
+    this.pload(
+      'https://calvin-absa.web.app/Eiffel-tower-paris.jpeg',
+      'https://calvin-absa.web.app/blog.jpeg',
+      'https://calvin-absa.web.app/flights.jpeg',
+      'https://calvin-absa.web.app/holidays.webp',
+      'https://calvin-absa.web.app/hotels.webp',
+      'https://calvin-absa.web.app/pyramids.webp',
+      'https://calvin-absa.web.app/taj.jpeg',
+    );
+  }
+
+  pload(...args: any[]): void {
+    for (let i = 0; i < args.length; i++) {
+      this.images[i] = new Image();
+      this.images[i].src = args[i];
+      console.log('loaded: ' + args[i]);
+    }
+  }
 }
